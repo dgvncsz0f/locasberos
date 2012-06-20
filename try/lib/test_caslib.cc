6887e0901299c2b3cf547eec3771ab905cc12cc1
dsouza <dsouza@bitforest.org>
2012-06-20 17:46:30 -0300

    bugfix: arch/debian compatibility

--- try/lib/test_caslib.cc
+++ try/lib/test_caslib.cc
@@ -28,7 +28,7 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <unittest++/UnitTest++.h>
+#include <UnitTest++.h>
 extern "C" {
 #include "alloca.h"
 #include "caslib.h"
