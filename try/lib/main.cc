9b1673880e016c3e8c2af0ca8695be03940752fe
PotHix <pothix@pothix.com>
2012-06-20 17:44:40 -0300

    Fixing unittest++ path.

--- try/lib/main.cc
+++ try/lib/main.cc
@@ -4,10 +4,10 @@
 // Copyright (c) 2012 pothix
 // Copyright (c) 2012 morellon
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-// 
+//
 //   * Redistributions of source code must retain the above copyright notice,
 //     this list of conditions and the following disclaimer.
 //   * Redistributions in binary form must reproduce the above copyright notice,
@@ -16,7 +16,7 @@
 //   * Neither the name of the <ORGANIZATION> nor the names of its contributors
 //     may be used to endorse or promote products derived from this software
 //     without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -28,7 +28,7 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <unittest++/UnitTest++.h>
+#include <UnitTest++.h>
 
 int main(void)
 {
