2d25b0185d07851c4d396d6db2a2c5adf22fdcd2
PotHix <pothix@pothix.com>
2012-06-22 12:38:11 -0300

    Fixing code styling and using input instead of output filters .

--- src/apache2_module/mod_locasberos.c
+++ src/apache2_module/mod_locasberos.c
@@ -41,113 +41,99 @@
 static const char s_szLocasberosName[]="Locasberos";
 module AP_MODULE_DECLARE_DATA locasberos_module;
 
-typedef struct
+typedef struct {
+  int bEnabled;
+} LocasberosConfig;
+
+static void *LocasberosCreateServerConfig(apr_pool_t *p,server_rec *s) {
+  LocasberosConfig *pConfig=apr_pcalloc(p,sizeof *pConfig);
+  pConfig->bEnabled=0;
+
+  return pConfig;
+}
+
+static void LocasberosInsertFilter(request_rec *r) {
+  LocasberosConfig *pConfig=ap_get_module_config(r->server->module_config,
+      &locasberos_module);
+
+  if(!pConfig->bEnabled)
+    return;
+
+  ap_add_input_filter(s_szLocasberosName,NULL,r,r->connection);
+}
+
+static apr_status_t LocasberosOutFilter(ap_filter_t *f, apr_bucket_brigade *pbbIn) {
+  request_rec *r = f->r;
+  conn_rec *c = r->connection;
+  apr_bucket *pbktIn;
+  apr_bucket_brigade *pbbOut;
+
+  pbbOut=apr_brigade_create(r->pool, c->bucket_alloc);
+  for (pbktIn = APR_BRIGADE_FIRST(pbbIn); pbktIn != APR_BRIGADE_SENTINEL(pbbIn); pbktIn = APR_BUCKET_NEXT(pbktIn)) {
+    const char *data;
+    apr_size_t len;
+    char *buf;
+    apr_size_t n;
+    apr_bucket *pbktOut;
+
+    if(APR_BUCKET_IS_EOS(pbktIn))
     {
-    int bEnabled;
-    } LocasberosConfig;
-
-static void *LocasberosCreateServerConfig(apr_pool_t *p,server_rec *s)
-    {
-    LocasberosConfig *pConfig=apr_pcalloc(p,sizeof *pConfig);
-
-    pConfig->bEnabled=0;
-
-    return pConfig;
+      apr_bucket *pbktEOS=apr_bucket_eos_create(c->bucket_alloc);
+      APR_BRIGADE_INSERT_TAIL(pbbOut,pbktEOS);
+      continue;
     }
 
-static void LocasberosInsertFilter(request_rec *r)
-    {
-    LocasberosConfig *pConfig=ap_get_module_config(r->server->module_config,
-                                                   &locasberos_module);
+    /* read */
+    apr_bucket_read(pbktIn,&data,&len,APR_BLOCK_READ);
 
-    if(!pConfig->bEnabled)
-        return;
+    /* write */
+    buf = apr_bucket_alloc(len, c->bucket_alloc);
+    for(n=0 ; n < len ; ++n)
+      buf[n] = apr_toupper(data[n]);
 
-    ap_add_output_filter(s_szLocasberosName,NULL,r,r->connection);
-    }
+    pbktOut = apr_bucket_heap_create(buf, len, apr_bucket_free,
+        c->bucket_alloc);
+    APR_BRIGADE_INSERT_TAIL(pbbOut,pbktOut);
+  }
 
-static apr_status_t LocasberosOutFilter(ap_filter_t *f,
-                                        apr_bucket_brigade *pbbIn)
-    {
-    request_rec *r = f->r;
-    conn_rec *c = r->connection;
-    apr_bucket *pbktIn;
-    apr_bucket_brigade *pbbOut;
+  /* Q: is there any advantage to passing a brigade for each bucket?
+   * A: obviously, it can cut down server resource consumption, if this
+   * experimental module was fed a file of 4MB, it would be using 8MB for
+   * the 'read' buckets and the 'write' buckets.
+   *
+   * Note it is more efficient to consume (destroy) each bucket as it's
+   * processed above than to do a single cleanup down here.  In any case,
+   * don't let our caller pass the same buckets to us, twice;
+   */
+  apr_brigade_cleanup(pbbIn);
+  return ap_pass_brigade(f->next,pbbOut);
+}
 
-    pbbOut=apr_brigade_create(r->pool, c->bucket_alloc);
-    for (pbktIn = APR_BRIGADE_FIRST(pbbIn);
-         pbktIn != APR_BRIGADE_SENTINEL(pbbIn);
-         pbktIn = APR_BUCKET_NEXT(pbktIn))
-    {
-        const char *data;
-        apr_size_t len;
-        char *buf;
-        apr_size_t n;
-        apr_bucket *pbktOut;
+static const char *LocasberosEnable(cmd_parms *cmd, void *dummy, int arg) {
+  LocasberosConfig *pConfig=ap_get_module_config(cmd->server->module_config,
+      &locasberos_module);
+  pConfig->bEnabled=arg;
 
-        if(APR_BUCKET_IS_EOS(pbktIn))
-            {
-            apr_bucket *pbktEOS=apr_bucket_eos_create(c->bucket_alloc);
-            APR_BRIGADE_INSERT_TAIL(pbbOut,pbktEOS);
-            continue;
-            }
+  return NULL;
+}
 
-        /* read */
-        apr_bucket_read(pbktIn,&data,&len,APR_BLOCK_READ);
+static const command_rec LocasberosCmds[] = {
+  AP_INIT_FLAG("Locasberos", LocasberosEnable, NULL, RSRC_CONF,
+      "Validate CAS authentication for this host"),
+  { NULL }
+};
 
-        /* write */
-        buf = apr_bucket_alloc(len, c->bucket_alloc);
-        for(n=0 ; n < len ; ++n)
-            buf[n] = apr_toupper(data[n]);
+static void LocasberosRegisterHooks(apr_pool_t *p) {
+  ap_hook_insert_filter(LocasberosInsertFilter,NULL,NULL,APR_HOOK_MIDDLE);
+  ap_register_input_filter(s_szLocasberosName,LocasberosOutFilter,NULL, AP_FTYPE_RESOURCE);
+}
 
-        pbktOut = apr_bucket_heap_create(buf, len, apr_bucket_free,
-                                         c->bucket_alloc);
-        APR_BRIGADE_INSERT_TAIL(pbbOut,pbktOut);
-        }
-
-    /* Q: is there any advantage to passing a brigade for each bucket?
-     * A: obviously, it can cut down server resource consumption, if this
-     * experimental module was fed a file of 4MB, it would be using 8MB for
-     * the 'read' buckets and the 'write' buckets.
-     *
-     * Note it is more efficient to consume (destroy) each bucket as it's
-     * processed above than to do a single cleanup down here.  In any case,
-     * don't let our caller pass the same buckets to us, twice;
-     */
-    apr_brigade_cleanup(pbbIn);
-    return ap_pass_brigade(f->next,pbbOut);
-    }
-
-static const char *LocasberosEnable(cmd_parms *cmd, void *dummy, int arg)
-    {
-    LocasberosConfig *pConfig=ap_get_module_config(cmd->server->module_config,
-                                                   &locasberos_module);
-    pConfig->bEnabled=arg;
-
-    return NULL;
-    }
-
-static const command_rec LocasberosCmds[] =
-    {
-    AP_INIT_FLAG("Locasberos", LocasberosEnable, NULL, RSRC_CONF,
-                 "Validate CAS authentication for this host"),
-    { NULL }
-    };
-
-static void LocasberosRegisterHooks(apr_pool_t *p)
-    {
-    ap_hook_insert_filter(LocasberosInsertFilter,NULL,NULL,APR_HOOK_MIDDLE);
-    ap_register_output_filter(s_szLocasberosName,LocasberosOutFilter,NULL,
-                              AP_FTYPE_RESOURCE);
-    }
-
-AP_DECLARE_MODULE(locasberos) =
-{
-    STANDARD20_MODULE_STUFF,
-    NULL,
-    NULL,
-    LocasberosCreateServerConfig,
-    NULL,
-    LocasberosCmds,
-    LocasberosRegisterHooks
+AP_DECLARE_MODULE(locasberos) = {
+  STANDARD20_MODULE_STUFF,
+  NULL,
+  NULL,
+  LocasberosCreateServerConfig,
+  NULL,
+  LocasberosCmds,
+  LocasberosRegisterHooks
 };
