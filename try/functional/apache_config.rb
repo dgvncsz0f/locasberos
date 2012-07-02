cc5464c5d92f999515b9a87b54af04ac2f6857cd
PotHix <pothix@pothix.com>
2012-07-02 01:55:47 -0300

    New test structure.

---
+++ try/functional/apache_config.rb
@@ -0,0 +1,86 @@
+require "spec/spec_helper"
+
+class ApacheConfig < LocasberosTestConfig
+
+  def base_file
+    File.expand_path(File.dirname(__FILE__) + "/apache.cfg.erb")
+  end
+
+  def apache_start(server_root)
+    cmd = $bin_httpd +
+          " -k start" +
+          " -C LogLevel\\ debug" +
+          " -e error" +
+          " -f "+ server_root +"/apache.cfg" +
+          " -d "+ server_root
+    IO.popen(cmd) { Process.wait }
+    25.times do
+      begin
+        TCPSocket.new(@listen_host, @listen_port).close()
+        return
+      rescue
+      end
+      sleep(0.2)
+    end
+    raise(RuntimeError.new("cant load apache ["+cmd+"]"))
+  end
+
+  def apache_stop(server_root)
+    cmd = $bin_httpd +
+          " -k stop" +
+          " -f "+ server_root +"/apache.cfg" +
+          " -d "+ server_root +
+          " 2>&1"
+    IO.popen(cmd) { Process.wait }
+    25.times do
+      begin
+        TCPSocket.new(@listen_host, @listen_port).close()
+      rescue
+        return
+      end
+      sleep(0.2)
+    end
+  end
+
+  def with_apache(&proc)
+    (root,cfg) = configure
+    errorlog   = "#{root}/logs/error.log"
+    begin
+      apache_start(root)
+      begin
+        proc.call()
+      ensure
+        apache_stop(root)
+      end
+    rescue
+      puts "\n> exceptiong caught..."
+      puts "> error.log: "
+      cat(errorlog).lines.each {|l| puts ">> #{l}"}
+      raise
+    ensure
+      clean(root)
+    end
+  end
+
+  def configure
+    tmp_root      = File.expand_path(File.dirname(__FILE__) + "/../tmp")
+    server_root   = Dir.mktmpdir(nil, tmp_root)
+
+    FileUtils.mkdir_p(server_root + "/logs")
+    FileUtils.mkdir_p(server_root + "/www")
+    FileUtils.mkdir_p(server_root + "/v_www")
+    FileUtils.cp(
+      url_for_fixture("/apache/passwd.db").gsub("file://",""),
+      server_root + "/passwd.db"
+    )
+    File.open(server_root + "/www/index.txt", "w") {|f1| f1.write("Ok") }
+    File.open(server_root + "/v_www/index.txt", "w") {|f1| f1.write("Ok") }
+    File.open(server_root + "/apache.cfg", "w") {|f1| f1.write(template) }
+
+    return([server_root, template])
+  end
+
+  def libexecdir
+    IO.popen("#{$bin_apxs} -q LIBEXECDIR") {|f| f.read.strip}
+  end
+end
---
+++ try/functional/locasberos_test_config.rb
@@ -0,0 +1,33 @@
+class LocasberosTestConfig
+  attr_accessor :listen_host, :listen_port, :root,
+                :xdir, :page, :enabled, :endpoint,
+                :cas_service, :auth_type, :require_type
+
+  def initialize
+    @root         = File.expand_path(File.dirname(__FILE__) + "/../../dist")
+    @listen_host  = "127.0.0.1"
+    @listen_port  = 9999
+
+    @enabled      = "On"
+    @endpoint     = url_for_fixture("/auth_failure")
+    @cas_service  = "undefined"
+    @auth_type    = "Locasberos"
+    @require_type = "valid-user"
+  end
+
+  def url_for(path)
+    "http://" + @listen_host + ":" + @listen_port.to_s + path
+  end
+
+  def url_for_fixture(path)
+    fixtures = File.expand_path(File.dirname(__FILE__) + "/../fixtures")
+    "file://#{fixtures}#{path}"
+  end
+
+  def template
+    ERB.new(File.read base_file).result(get_binding)
+  end
+
+  # class binding to use on ERB
+  def get_binding; binding; end
+end
